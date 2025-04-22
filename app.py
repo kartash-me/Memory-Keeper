195f82c824ca4916b8f8f3355187ac6ab8819238
kartash <kartashovAlex2008@yandex.ru>
2025-04-23 00:30:17 +0300

    🔄 refactor: streamline forms and enhance registration logic
    - Improved form structure in `forms.py`:
      * Introduced common validator variables for reusability.
      * Simplified repetitive validator definitions across forms.
    
    - Enhanced registration flow in `app.py`:
      * Added session-based step management for multi-step registration.
      * Implemented session cleanup and user ID assignment upon completion.
      * Updated form handling logic for better readability and scalability.
    
    - Minor adjustments:
      * Standardized quotes in route methods.
      * Cleared session in the logout function for improved security.
    
    This refactor improves code clarity, reduces duplication, and enhances the multi-step registration process.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, render_template
+from flask import Flask, redirect, render_template, session
 from flask_login import LoginManager, login_required, logout_user
 
 from data import db_session
@@ -7,7 +7,7 @@ from forms import EmailStepForm, FinalStepForm, LoginForm, PhoneStepForm
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your_secret_key'
+app.config["SECRET_KEY"] = "your_secret_key"
 
 login_manager = LoginManager()
 login_manager.init_app(app)
@@ -26,28 +26,40 @@ def index():
     return render_template("base.html", title="Memory Keeper")
 
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
     return render_template("login.html", title="Вход", form=form)
 
 
-@app.route('/register', methods=['GET', 'POST'])
+@app.route("/register", methods=["GET", "POST"])
 def register():
-    forms = {"0": PhoneStepForm(), "1": EmailStepForm(), "2": FinalStepForm()}
-    form = forms.get("0")
+    if session.get("user_id"):
+        return "data from form has been received"
+
+    forms = [PhoneStepForm(), EmailStepForm(), FinalStepForm()]
+    form = forms[session.get("step", 0)]
 
     if form.validate_on_submit():
-        form = forms.get(form.step.data)
+        session["step"] = session.get("step", 0) + 1
+
+        if session["step"] == 3:
+            # здесь должна быть логика добавления пользователя в БД
+            session["user_id"] = 1 # а здесь соответствующий ID
+            session.pop("step")
+            return "data from form has been received"
+
+        form = forms[session["step"]]
 
     return render_template("register.html", title="Регистрация", form=form)
 
 
-@app.route('/logout')
+@app.route("/logout")
 @login_required
 def logout():
     logout_user()
-    return redirect('/')
+    session.clear()
+    return redirect("/")
 
 
 if __name__ == "__main__":
